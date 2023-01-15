7854126c06a525c2a630ccc7e4705f2175de18c9
danielacmg <dcmg.ucv@gmail.com>
2023-01-15 19:29:28 +0000

    Changing Not working ico

--- src/js/component/Jumbotron/jumbotron.jsx
+++ src/js/component/Jumbotron/jumbotron.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Jumbotron = (props) => {
 	return (
             <div class="jumbotron bg-light p-2">
-                <h1 class="display-4">It Work!</h1>
+                <h1 class="display-4">It Works! =)</h1>
                 
                 <p>{props.jumbotronText}</p>
                 <p class="lead">
