170065feaf395eb6ef4acb303dac44a57c5ac734
danielacmg <dcmg.ucv@gmail.com>
2023-01-15 21:04:58 +0000

    Solving references problems

--- src/js/component/Card/card.jsx
+++ src/js/component/Card/card.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
+import ImgTest from "./big-project-image.png"
 
 const Card = (props) => {
 	return (        
         <div class="col-sm-4">
             <div class="card m-2" >
-                <img class="card-img-top" src="big-project-image.png" alt="Card image cap"/>
+                <img class="card-img-top" src={ImgTest} alt="Card image cap"/>
                 <div class="card-body p-2">
                     <h5 class="card-title">{props.cardTitle}</h5>
                     <p class="card-text">{props.cardText}</p>
